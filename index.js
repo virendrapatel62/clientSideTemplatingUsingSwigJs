e5f6d0fb7efee121973e4d3b07355d69e5b70b07
virendra <patelvirendra62@gmail.com>
2021-07-17 19:49:44 +0530

    first_example

---
+++ index.js
@@ -0,0 +1,36 @@
+const user = {
+    name: 'Virendra',
+    age: 23,
+    address: 'jabalpur MP'
+}
+
+window.onload = () => {
+    console.log("Window is loaded...")
+
+    const info = document.querySelector("#info")
+
+    const templateElement = document.querySelector("#template")
+    const templateHtml = templateElement.innerHTML
+    console.log({ templateHtml, swig })
+
+    const template = swig.compile(templateHtml)
+    const html = template(user)
+    console.log(html);
+
+
+    info.innerHTML = html
+
+    // const name = document.createElement("h1")
+    // const age = document.createElement("h2")
+    // const address = document.createElement("p")
+
+    // name.innerHTML = user.name
+    // age.innerHTML = user.age
+    // address.innerHTML = user.address
+
+    // const info = document.querySelector("#info")
+    // info.appendChild(name)
+    // info.appendChild(age)
+    // info.appendChild(address)
+
+}
